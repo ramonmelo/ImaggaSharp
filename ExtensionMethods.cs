5ebb77ea08198160a8155709963a96faa71c25fc
Ramon Melo <ramonmelo@ramonmelo.com>
2021-07-29 21:54:34 -0300

    Added Extension Methods

---
+++ ExtensionMethods.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PhotoTagger
+{
+    public static class ExtensionMethods
+    {
+        public static string ToStringExtended(this object obj)
+        {
+            var type = obj.GetType();
+
+            var props = type.GetProperties();
+            var fields = type.GetFields();
+
+            var sb = new StringBuilder("[");
+
+            // Header
+            sb.Append(type.Name).Append(": ");
+
+            foreach (var p in props)
+            {
+                sb.Append(p.Name).Append("=").Append(p.GetValue(obj, null)).Append(", ");
+            }
+
+            foreach (var f in fields)
+            {
+                sb.Append(f.Name).Append("=").Append(f.GetValue(obj)).Append(", ");
+            }
+
+            // Tail
+            sb.Append("]");
+
+            return sb.ToString();
+        }
+    }
+}
