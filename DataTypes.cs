17f81b210defa1ad873cf6c38a563b7a90911a48
Ramon Melo <ramonmelo@ramonmelo.com>
2021-07-29 21:54:18 -0300

    Added Imagga Tag Data Types

---
+++ DataTypes.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PhotoTagger
+{
+    internal class Status
+    {
+        public string text;
+        public string type;
+
+        public bool IsError => type.Equals("error");
+        public bool IsSuccess => type.Equals("success");
+
+        public override string ToString()
+        {
+            return this.ToStringExtended();
+        }
+    }
+
+    internal class Tag
+    {
+        public float confidence;
+        public Dictionary<string, string> tag;
+        
+        public override string ToString()
+        {
+            return this.ToStringExtended();
+        }
+    }
+
+    internal class Result
+    {
+        public List<Tag> tags;
+
+        public override string ToString()
+        {
+            return this.ToStringExtended();
+        }
+    }
+
+    internal class TagResult
+    {
+        public Result result;
+        public Status status;
+
+        public override string ToString()
+        {
+            return this.ToStringExtended();
+        }
+    }
+}
